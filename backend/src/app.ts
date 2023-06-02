f214fa5b5ca601418288cf2d66a38b3bbbd6bd6d
Igor Nunes <igor@tseguros.com.br>
2023-06-01 21:00:24 -0300

    Fix: routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,16 @@
 import "./bootstrap";
 import express from 'express';
 import bodyParser from 'body-parser';
-import router from './router';
+import routes from "./routes";
 
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(router)
+app.use(routes)
 
 app.listen(process.env.PORT, () => {
     console.log(`Server started on port: ${process.env.PORT}`)
 });
+
+export default app;
