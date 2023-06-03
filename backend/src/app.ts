ac9a9bf7827d4279501321a906a27a909ca28066
Igor Nunes <igor@tseguros.com.br>
2023-06-03 11:20:32 -0300

    add: cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,20 @@
 import "./bootstrap";
 import express from 'express';
+import cors from "cors";
 import bodyParser from 'body-parser';
 import routes from "./routes";
 
 
 const app = express();
+app.use(
+    cors({
+        credentials: true,
+        origin: [
+            "http://localhost:4200",
+        ],
+    })
+);
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(routes)
