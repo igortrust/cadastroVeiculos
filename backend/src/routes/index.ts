83fd490ee442b365aacf3a20c23189191d577827
Igor Nunes <igor@tseguros.com.br>
2023-06-03 14:49:32 -0300

    add: swagger

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
+import swaggerUi from "swagger-ui-express";
+import * as swaggerDocument from "../swagger.json"
+
 
 import veiculoRoutes from "./veiculoRoutes";
 
 const routes = Router();
 
 routes.use(veiculoRoutes)
+routes.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 export default routes;
